79e5cc0ecb87d35f34e088df81a9235c601aba22
Michał Nowakowski <michal951@poczta.onet.pl>
2019-09-19 11:44:06 +0200

    add tests for multiplying and adding numbers in an array

--- src/test/java/CalculatorTests.java
+++ src/test/java/CalculatorTests.java
@@ -24,6 +24,7 @@ public class CalculatorTests {
         //given
         Calculator calculator = new Calculator();
 
+
         //then
         Assert.assertNotEquals(50, calculator.add(50,50));
     }
@@ -74,6 +75,42 @@ public class CalculatorTests {
 
         //then
         Assert.assertNotEquals(2,calculator.divide(100,0));
+    }
+
+    @Test
+    public void should_multiply_two_numbers(){
+
+        //given
+        Calculator calculator = new Calculator();
+
+        //then
+        Assert.assertEquals(50, calculator.multiply(25,2));
+        Assert.assertEquals(50, calculator.multiply(2,25));
+        Assert.assertEquals(0, calculator.multiply(0,0));
+        Assert.assertEquals(0, calculator.multiply(25,0));
+        Assert.assertEquals(-50, calculator.multiply(25,-2));
+        Assert.assertEquals(10, calculator.multiply(-5,-2));
+    }
+
+    @Test
+    public void should_not_multiply_two_numbers(){
+
+        //given
+        Calculator calculator = new Calculator();
+
+        //then
+        Assert.assertNotEquals(100, calculator.multiply(50,50));
+    }
+
+    @Test
+    public void should_add_all_numbers_from_array(){
+
+        //given
+        Calculator calculator = new Calculator();
+        int[] numbers = {5,4,3,2,1,0};
+
+        //then
+        Assert.assertEquals(15,calculator.add(numbers));
     }
 
 }
